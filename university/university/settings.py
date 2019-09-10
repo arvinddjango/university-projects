059856134841a27f220194f3015b076f3cd4ab76
arvinddjango <49224894+arvinddjango@users.noreply.github.com>
2019-09-11 01:13:36 +0530

    Update settings.py

--- university/university/settings.py
+++ university/university/settings.py
@@ -83,17 +83,22 @@ WSGI_APPLICATION = 'university.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'webs4solution',
+#         'USER': 'webs',
+#         'PASSWORD': 'Arvind@1432',
+#         'HOST': 'localhost',
+#         'PORT': '3306'
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'webs4solution',
-        'USER': 'webs',
-        'PASSWORD': 'Arvind@1432',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
